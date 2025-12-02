8a459d2e31779aaf268677e7ef61b82e3357899b
br <briandevj@gmail.com>
2025-12-02 16:37:56 -0500

    .

--- web/routing/router.go
+++ web/routing/router.go
@@ -3,7 +3,11 @@ package routing
 import "net/http"
 
 type Router interface {
+	// ServeHTTP is invoked before invoking the route-matched handler's ServeHTTP
+	// for every request regardless of the request url.
+	// This can be easily implemented by embedding *http.ServeMux
 	ServeHTTP(w http.ResponseWriter, r *http.Request)
+
 	Handle(pattern string, handler http.Handler, handlerWrappers ...HandlerWrapper)
 	HandleFunc(pattern string, handleFunc func(http.ResponseWriter, *http.Request), handlerWrappers ...HandlerWrapper)
 	Group(prefix string, batch func(*RouteGroup), handlerWrappers ...HandlerWrapper) *RouteGroup
