9b709f8d64fb8a0b01a5087fb24b93d45e4c0601
br <briandevj@gmail.com>
2025-12-02 16:02:02 -0500

    .

--- web/middle/recover_panic.go
+++ web/middle/recover_panic.go
@@ -8,7 +8,7 @@ import (
 	"github.com/logitools/gw/web/responses"
 )
 
-func RecoverWrapper(inner http.Handler) http.Handler {
+func RecoverPanic(inner http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if rec := recover(); rec != nil {
