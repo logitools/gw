a49a5cad66112930a15de2a3b9bf773a7d22ab44
br <briandevj@gmail.com>
2025-12-04 12:52:32 -0500

    .

--- framework/core_svc_web.go
+++ framework/core_svc_web.go
@@ -6,6 +6,8 @@ import (
 	"github.com/logitools/gw/web"
 )
 
+// PrepareWebService
+// Call this when all the required services are prepared
 func (c *Core[B]) PrepareWebService(addr string, httpHandler http.Handler) {
 	c.WebService = web.NewService(c.RootCtx, addr, httpHandler)
 	c.AddService(c.WebService)
