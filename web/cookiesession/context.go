d090075a26ac3ed006f48492256ee2b02d577069
br <briandevj@gmail.com>
2025-12-10 18:37:13 -0500

    .

--- web/cookiesession/context.go
+++ web/cookiesession/context.go
@@ -2,6 +2,9 @@ package cookiesession
 
 import (
 	"context"
+	"strconv"
+
+	"github.com/logitools/gw/authuser"
 )
 
 type idCtxKey struct{}
@@ -14,4 +17,13 @@ func WebSessionIdFromContext(ctx context.Context) (string, bool) {
 	ctxVal := ctx.Value(idCtxKey{})
 	val, ok := ctxVal.(string)
 	return val, ok
+}
+
+func SessionIDToInt64UIDCtxInjector(ctx context.Context, sessionID string, uidStr string) (context.Context, error) {
+	ctx = WithWebSessionId(ctx, sessionID)
+	uid, err := strconv.ParseInt(uidStr, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return authuser.WithUserID[int64](ctx, uid), nil
 }
